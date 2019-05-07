5d3a8cf2ccc3dcd7c4437be465bb421e0d9ca387
John Hurliman <jhurliman@jhurliman.org>
2019-05-07 01:04:18 -0700

    Syntax fixes

--- video-led-driver.cpp
+++ video-led-driver.cpp
@@ -13,14 +13,14 @@ constexpr size_t ROWS = 300;
 constexpr size_t LED_COUNT = 300;
 constexpr size_t BORDER_SIZE = 3;
 constexpr double SCALE_FACTOR = 1.05;
-constexpr double FPS = 30.0;
-constexpr cv::Size RESIZE_SIZE(COLS + BORDER_SIZE * 2, ROWS + BORDER_SIZE * 2);
-constexpr cv::Rect CROP_RECT(BORDER_SIZE, BORDER_SIZE, COLS, ROWS);
-constexpr std::chrono::milliseconds FRAME_TIME(1000.0 / FPS);
-
+constexpr size_t FPS = 30;
+constexpr std::chrono::milliseconds FRAME_TIME(1000 / FPS);
 constexpr int DMA = 10;
 constexpr int GPIO_PIN = 18;
 constexpr int LED_COUNT = 300;
+
+static const cv::Size RESIZE_SIZE(COLS + BORDER_SIZE * 2, ROWS + BORDER_SIZE * 2);
+static const cv::Rect CROP_RECT(BORDER_SIZE, BORDER_SIZE, COLS, ROWS);
 
 bool gRunning = true;
 ws2811_t gLEDs = {
@@ -28,29 +28,29 @@ ws2811_t gLEDs = {
     .dmanum = DMA,
     .channel = {
         [0] = {
-            .gpionum = GPIO_PIN,
-            .count = LED_COUNT,
-            .invert = 0,
-            .brightness = 255,
-            .strip_type = WS2811_STRIP_GRB,
+            gpionum : GPIO_PIN,
+            count : LED_COUNT,
+            invert : 0,
+            brightness : 255,
+            strip_type : WS2811_STRIP_GRB,
         },
         [1] = {
-            .gpionum = 0,
-            .count = 0,
-            .invert = 0,
-            .brightness = 0,
+            gpionum : 0,
+            count : 0,
+            invert : 0,
+            brightness : 0,
         },
     },
 };
 
-static std::string pixelToString(const Vec3b pixel) {
+static std::string pixelToString(const cv::Vec3b pixel) {
     auto r = pixel.val[0];
     auto g = pixel.val[1];
     auto b = pixel.val[2];
     return "<" + std::to_string(r) + ", " + std::to_string(g) + ", " + std::to_string(b) + ">";
 }
 
-static ws2811_led_t pixelToLEDColor(const Vec3b pixel) {
+static ws2811_led_t pixelToLEDColor(const cv::Vec3b pixel) {
     uint8_t red = pixel.val[0];
     uint8_t green = pixel.val[1];
     uint8_t bblue = pixel.val[2];
@@ -67,8 +67,7 @@ static void clearLEDs() {
     }
 }
 
-int main(int, char**)
-{
+int main(int, char**) {
     std::signal(SIGINT, signalHandler);
     std::signal(SIGTERM, signalHandler);
 
