89c454e647b7096b9da7a7ff37ae12e733562fd6
John Hurliman <jhurliman@jhurliman.org>
2019-05-08 00:22:22 -0700

    Build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p dist
+g++ \
+  -std=c++17 \
+  -lopencv_imgproc \
+  -lopencv_highgui \
+  -lopencv_core \
+    video-led-driver.cpp \
+    rpi_ws281x/libws2811.a \
+  -o dist/video-led-driver
